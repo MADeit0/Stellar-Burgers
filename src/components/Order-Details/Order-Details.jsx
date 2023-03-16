6cb7f2702e2f57eed28e9da1ed95ec9a508a11ef
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-03-16 20:13:35 +0300

    fix: add alt attribute for component img OrderDetails

--- src/components/Order-Details/Order-Details.jsx
+++ src/components/Order-Details/Order-Details.jsx
@@ -4,7 +4,7 @@ const OrderDetails = () => (
   <>
     <h3 className="text text_type_digits-large pt-30 pb-8">034536</h3>
     <p className="text text_type_main-medium pb-15">идентификатор заказа</p>
-    <img src={done} alt="" />
+    <img src={done} alt="картинка успешного заказа" />
     <p className="text text_type_main-small pt-15 pb-2">
       Ваш заказ начали готовить
     </p>

38025c25cb5ccca2515528deab47e240136f8c3d
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-07-15 11:53:54 +0300

    feat: remove the error when deploying to gh-pages

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename="/Stellar-Burgers">
         <App />
       </Router>
     </Provider>

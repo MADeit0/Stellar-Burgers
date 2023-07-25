25695878c37eb2557737989a90992950da7e4d59
Олег Цыпуштанов <madeit@MacBook-Pro-Oleg.local>
2023-07-25 22:03:27 +0300

    feat: create and customize logic on the order page

--- src/pages/OrdersPage/OrdersPage.jsx
+++ src/pages/OrdersPage/OrdersPage.jsx
@@ -1,3 +1,38 @@
+import ordersStyle from "./OrdersPage.module.css";
+
+import { useDispatch, useSelector } from "react-redux";
+import { wsFeedsActions } from "../../store/wsFeeds/wsFeedsSlice";
+import ScrollBar from "../../components/ScrollBar/ScrollBar";
+import OrderElement from "../../components/OrderElement/OrderElement";
+import { nanoid } from "@reduxjs/toolkit";
+import { useLayoutEffect } from "react";
+
 export default function OrdersPage() {
-  return <section>Тут будет история заказов</section>;
+  const dispatch = useDispatch();
+
+  const ordersList = useSelector(({ wsFeeds }) => wsFeeds.data?.orders);
+  const success = useSelector(({ wsFeeds }) => wsFeeds.data?.success);
+
+  useLayoutEffect(() => {
+    dispatch(wsFeedsActions.startConnecting());
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className={ordersStyle.container}>
+      <ScrollBar>
+        {ordersList.map((order) => (
+          <OrderElement
+            key={nanoid()}
+            wsSuccess={success}
+            ingredients={order.ingredients}
+            status={order.status}
+            number={order.number}
+            name={order.name}
+            createdAt={order.createdAt}
+          />
+        ))}
+      </ScrollBar>
+    </div>
+  );
 }
